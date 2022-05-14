eb85c6d171d9c23bbf3f27a96ae6ed92b7b63bce
Methma-Vindula-Wijesooriya <methma.vindula@gmail.com>
2022-05-14 11:15:00 -0700

    navigation

---
+++ assets/js/KVC.js
@@ -0,0 +1,16 @@
+$('.mv-events-news-slick').slick({
+
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 3,
+
+
+});
+
+$('.mv-banner-slick').slick({
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1
+
+});
