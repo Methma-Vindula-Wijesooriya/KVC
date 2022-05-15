eddc56d1ec85cd4ff1088142875263dfb5e4cae7
Methma-Vindula-Wijesooriya <methma.vindula@gmail.com>
2022-05-15 07:38:42 -0700

    responsive

--- assets/js/KVC.js
+++ assets/js/KVC.js
@@ -2,6 +2,31 @@ $('.mv-events-news-slick').slick({
     infinite: true,
     slidesToShow: 4,
     slidesToScroll: 3,
+    responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+
+        }
+    ]
 });
 
 
@@ -19,4 +44,30 @@ $('.mv-navi-slick').slick({
     infinite: true,
     slidesToShow: 4,
     slidesToScroll: 3,
+    responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+
+        }
+    ]
+
 });
